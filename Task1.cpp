73f22333981c8070d662a81e0f5c4d6eb06a86be
iimerr <emirnakbelajar@gmail.com>
2025-06-15 16:31:04 +0800

    innitial commit

---
+++ Task1.cpp
@@ -0,0 +1,50 @@
+/*
+ID = 24003510
+NAME = Emir AZimil AKbar Bin Mohd Fauzi
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    int id;
+    string contact;
+    string email;
+};
+
+int main() {
+    const int numStudents = 5;
+    Student students[numStudents];
+
+    // Input student data
+    cout << "Enter data for 5 students:\n";
+    for (int i = 0; i < numStudents; ++i) {
+        cout << "\nStudent " << (i + 1) << ":\n";
+        cout << "Name: ";
+        getline(cin, students[i].name);
+
+        cout << "ID: ";
+        cin >> students[i].id;
+        cin.ignore(); // clear newline character from buffer
+
+        cout << "Contact: ";
+        getline(cin, students[i].contact);
+
+        cout << "Email: ";
+        getline(cin, students[i].email);
+    }
+
+    // Display student data
+    cout << "\nStored Student Data:\n";
+    for (int i = 0; i < numStudents; ++i) {
+        cout << "\nStudent " << (i + 1) << ":\n";
+        cout << "Name: " << students[i].name << endl;
+        cout << "ID: " << students[i].id << endl;
+        cout << "Contact: " << students[i].contact << endl;
+        cout << "Email: " << students[i].email << endl;
+    }
+
+    return 0;
+}
